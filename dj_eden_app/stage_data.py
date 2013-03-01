028c4ab53d041e8c13c4e65a712da6da31626909
R Hayes <rhayes@usgs.gov>
2013-03-01 00:45:23 -0600

    Alchemical query cleaned up

--- dj_eden_app/stage_data.py
+++ dj_eden_app/stage_data.py
@@ -24,20 +24,13 @@ if __name__ == "__main__":
                stage.c.datetime.between('2008-03-01', '2008-03-02'))
     print s
 
+    print "*results"
     rs = engine.execute(s)
+    try:
+        for r in prepend(columnNames, rs):
+            print ",".join(map(str, r))
+    finally:
+        rs.close()
 
-    for r in rs:
-        print r
     print
-
-    print "**raw query"
-    rs = engine.execute("select datetime, stage_2A300 from stage where datetime between '2002-01-01' and '2002-01-02'")
-    for i in prepend(["datetime", "2A300"], rs):
-        print ",".join(map(str, i))
-    print
-
-    print "**raw query formatted by comprehension"
-    rs = engine.execute("select datetime, stage_2A300 from stage where datetime between '2002-01-02' and '2002-01-03'")
-    for i in prepend(["datetime", "2A300"], rs):
-        print ",".join(['"%s"' % x for x in i])
 
