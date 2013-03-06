d337822b0e0b7be0a894636b109bcc8b617de0bd
R Hayes <rhayes@usgs.gov>
2013-03-06 14:55:06 -0600

    Cleaned up queries.

--- dj_eden_app/stage_data.py
+++ dj_eden_app/stage_data.py
@@ -27,6 +27,16 @@ def data_for_plot(stations, **kwargs):
     q = _query_for_plot(stations, **kwargs)
     return q.execute()
 
+def data_for_download(stations, **kwargs):
+    """
+    Produces an iterable intended for download.
+    First column will be observation times, as datetime.datetime instances.
+    Every gage in the stations list will produce two columns: one for the verified observations, named like stage_GAGENAME,
+    another for the flag, flag_GAGENAME.
+    """
+    q = _query_for_download(stations, **kwargs)
+    return q.execute()
+
 def _query_for_plot(stations, beginDate=None, endDate=None, maxCount=None, navd88Offset={}):
     sel = select([stage.c.datetime])
 
@@ -36,11 +46,10 @@ def _query_for_plot(stations, beginDate=None, endDate=None, maxCount=None, navd8
         offset = navd88Offset.get(gage) or 0
         sel = sel.column(func.if_(stage.c[flag_name] == None,
                                   func.if_(stage.c[stage_name] == None, None, offset + stage.c[stage_name]),
-                                  None).label(stage_name))
-        est_stage_name = stage_name + " est"
+                                  None).label(gage))
         sel = sel.column(func.if_(stage.c[flag_name] != None,
                                   func.if_(stage.c[stage_name] == None, None, offset + stage.c[stage_name]),
-                                  None).label(est_stage_name))
+                                  None).label(gage + " est"))
 
     if beginDate is not None:
         sel = sel.where(stage.c.datetime >= beginDate)
@@ -62,8 +71,8 @@ def _query_for_download(stations, beginDate=None, endDate=None, navd88Offset={})
         stage_name = "stage_" + gage
         flag_name = "flag_" + gage
         offset = navd88Offset.get(gage) or 0
-        sel = sel.column(offset + stage.c[stage_name])
-        sel = sel.column(offset + stage.c[flag_name])
+        sel = sel.column(func.if_(stage.c[stage_name] == None, None, offset + stage.c[stage_name]).label(stage_name))
+        sel = sel.column(stage.c[flag_name])
 
     if beginDate is not None:
         sel = sel.where(stage.c.datetime >= beginDate)
