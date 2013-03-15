541a12fb3ab626dff549a4e7437fea54662cfb5c
Andrew Yan <ayan@usgs.gov>
2013-03-15 14:57:01 -0500

    revert to previous version of hydrographs.py

--- dj_eden_app/hydrograph.py
+++ dj_eden_app/hydrograph.py
@@ -9,7 +9,6 @@ matplotlib.use('Cairo')
 from matplotlib.pyplot import savefig, figure, plot_date, legend, xticks, axes, axhline, xlim, xlabel, ylabel
 from dj_eden_app.text_export import _generate_error_file
 from dj_eden_app.models import Station
-import dj_eden_app.data_queries as data_queries
 
 import textwrap
 
@@ -62,7 +61,7 @@ def plot_many(data, destination, begin_date, end_date, gage_list):
 
     figure()
     axes([0.1, 0.3, 0.5, 0.5])
-    plot_date(xList, yList, '-d', markersize=2.5)
+    plot_date(xList, yList, 'o', markersize = 2.5)
     xlabel('Date')
     ylabel('Water Level (NAVD88 ft)')
     if beginDate != None and endDate != None:
@@ -77,62 +76,20 @@ def plot_many(data, destination, begin_date, end_date, gage_list):
 
     return len(xList)
 
-_line_style_dict = {
+_line_style_ = {
                 'D': ":^",  # dotted with triangles
                 'O': "-d",  # solid with diamonds
                 'E': ":+",  # dotted with crosses
                 'M': " ",  # blank
                 }
-_line_styles = ["-d", ":+", ":+"]
-_line_colors = [
-"#000000",
-"#670075",
-"#830094",
-"#3800a3",
-"#0000c1",
-"#0025dd",
-"#007ddd",
-"#009adb",
-"#00aaab",
-"#00aa8d",
-"#009e28",
-"#00ac00",
-"#00ca00",
-"#00e700",
-"#1dff00",
-"#bcff00",
-"#ecef00",
-"#fcd200",
-"#ffa900",
-"#ff4500",
-"#f10000",
-"#d80000",
-"#cc1c1c",
-"#cccccc",
-]
 
 def line_style(flag):
-    return _line_style_dict.get(flag) or "-"
+    return _line_style_.get(flag) or "-"
 
 def png(data, destination, beginDate, endDate, gage_list):
 
     ct = plot_many(data, destination, beginDate, endDate, gage_list)
     savefig(destination, format="png")
-
-    return ct
-
-def png_multi(data, outfile, beginDate, endDate):
-    "Plot multiline onto outfile. Data has columns WHEN then O E D for each well."
-    ct = plot_multi(data, beginDate, endDate)
-    savefig(outfile, format="png")
-
-    return ct
-
-def png_single(data, outfile, beginDate=None, endDate=None, dry_elevation=None, ground_elevation=None):
-    "Plot single-well data series. Data has columns WHEN, O, E, D."
-
-    ct = plot_single(data, beginDate=beginDate, endDate=endDate, dry_elevation=dry_elevation, ground_elevation=ground_elevation)
-    savefig(outfile, format="png")
 
     return ct
 
@@ -143,16 +100,14 @@ if __name__ == "__main__":
     print "hg1.png", ct
 
     data = stage_data.data_for_plot(['2A300', 'G-3567'], maxCount=600)
-    ct = png(data, "/tmp/hg2.png", None, None)
+    ct = png(data, "/tmp/hg2.png")
     print "hg2.png", ct
 
     data = stage_data.data_for_plot(['2A300', 'G-3567'], beginDate="2004-01-01", endDate="2004-03-01", maxCount=600)
-    ct = png(data, "/tmp/hg3.png", dateutil.parser.parse("2004-01-01"), dateutil.parser.parse("2004-03-01"))
+    ct = png(data, "/tmp/hg3.png")
     print "hg3.png", ct
 
     data = stage_data.data_for_plot(['L31NN', 'Chatham_River_near_the_Watson_Place'], beginDate="2011-09-01", endDate="2011-12-31",
              maxCount=600)
-    ct = png(data, "/tmp/hg4.png", dateutil.parser.parse("2011-09-01"), dateutil.parser.parse("2011-12-31"))
+    ct = png(data, "/tmp/hg4.png")
     print "hg4.png", ct
-
-
