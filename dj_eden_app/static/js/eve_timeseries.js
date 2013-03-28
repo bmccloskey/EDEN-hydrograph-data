746c7d6262bdd695de34de6c89fb531ed4d6ad90
Andrew Yan <ayan@usgs.gov>
2013-03-28 10:38:50 -0500

    returned to original dygraphs resizing solution.

--- dj_eden_app/static/js/eve_timeseries.js
+++ dj_eden_app/static/js/eve_timeseries.js
@@ -1,21 +1,3 @@
-//snippet from stackoverflow -- avoids the reload from being fired multiple times during window resizing
-var waitForFinalEvent = (function () {
-  var timers = {};
-  return function (callback, ms, uniqueId) {
-    if (!uniqueId) {
-      uniqueId = "Don't call this twice without a uniqueId";
-    }
-    if (timers[uniqueId]) {
-      clearTimeout (timers[uniqueId]);
-    }
-    timers[uniqueId] = setTimeout(callback, ms);
-  };
-})();
-
-$(document).ready(function() {
-	$(window).resize(function() {
-		waitForFinalEvent(function() {
-			location.reload();
-		}, 500, "ts_dygraph_id");
-	});
+$(window).resize(function() {
+	location.reload();
 });
