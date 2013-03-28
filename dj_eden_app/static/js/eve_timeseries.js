0345cdc49c30ab9cf5f995f6b1363225c1ff342b
Andrew Yan <ayan@usgs.gov>
2013-03-28 15:17:36 -0500

    review with Mary, committing changes

--- dj_eden_app/static/js/eve_timeseries.js
+++ dj_eden_app/static/js/eve_timeseries.js
@@ -1,3 +1,21 @@
-$(window).resize(function() {
-	location.reload();
+//snippet from stackoverflow -- avoids the reload from being fired many times during window resizing
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
+$(document).ready(function() {
+	$(window).resize(function() {
+		waitForFinalEvent(function() {
+			location.reload();
+		}, 500, "ts_dygraph_id");
+	});
 });
