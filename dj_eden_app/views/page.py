0dee46bb1af72f4b2cf8be02d092b2db20c5c9e2
Andrew Yan <ayan@usgs.gov>
2013-03-28 16:17:46 -0500

    added clean method to timeseries form to make sure start date precedes the end date

--- dj_eden_app/views/page.py
+++ dj_eden_app/views/page.py
@@ -89,10 +89,6 @@ def eden_page(request):
             else:
                 str_tend = None
 
-            if query_form.cleaned_data['timeseries_start'] and query_form.cleaned_data['timeseries_end']:
-                time_delta = query_form.cleaned_data['timeseries_end'] - query_form.cleaned_data['timeseries_start']
-                time_delta_days = time_delta.days
-
             gages = query_form.cleaned_data['site_list']
             colors = ColorRange(count=len(gages))
 
@@ -108,7 +104,6 @@ def eden_page(request):
                                                    'color_list': list(colors),
                                                    'DYGRAPH_RANGE_SELECTOR':settings.DYGRAPH_RANGE_SELECTOR,
                                                    #'EDEN_URL': settings.EDEN_URL,
-                                                   'time_delta': time_delta_days,
 			}
             if len(gages) == 1:
                 station = data_queries.station_list(gages)[0]
